07a465e5ebd7a7daa6120d2a879a19e90f1152f4
Kane <kane.a.s.morgan@gmail.com>
2014-07-24 11:10:53 +0100

    changed name of groups column to group_name because of sql reserved word group

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140723133134) do
+ActiveRecord::Schema.define(version: 20140724094849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20140723133134) do
   create_table "contacts", force: true do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "group"
+    t.string   "group_name"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
