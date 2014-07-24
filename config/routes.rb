654511bf698d7c949200af9496e330ea6afa9a35
Matt Nicks <lighthouse_323@hotmail.com>
2014-07-24 09:06:03 +0100

    Log out links fixed

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     resources :contacts
   end
   resources :sessions  
+  match '/signout',  to: 'sessions#destroy',     via: 'delete'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
